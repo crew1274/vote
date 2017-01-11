4a02243d52f509785d24b392926cbfdfc3070865
黃偉鑫 <crew1274@yahoo.com.tw>
2017-01-12 00:40:37 +0800

    Update conn.inc.php

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,21 +1,10 @@
 <?php
 	 //数据库连接文件
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '50150');
-	define('DB_NAME', 'vt_vote');
-
-	//1.连接数据库
-
-	/*用在本地测试*/
+	define('DB_USER', '');
+	define('DB_PASS', '');
+	define('DB_NAME', '');
 	$conn = @mysql_connect(DB_HOST,DB_USER,DB_PASS) or die('資料庫連接失敗!');
-	/*用在SAE测试*/
-	//$conn = @mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS) or die('数据库连接失败!');
-	//2.选择数据库
-	/*用在本地测试*/
 	mysql_select_db(DB_NAME) or die('資料庫不存在!');
-	/*用在SAE测试*/
-	//mysql_select_db(SAE_MYSQL_DB) or die('数据库不存在!');
-	//3.设置字符集
 	mysql_query("SET NAMES UTF8");
 ?>
